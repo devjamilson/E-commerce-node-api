623178be9f164d2bed9fd2c4fbe2d6c1090701eb
Jamilson Ferreira <83456401+devjamilson@users.noreply.github.com>
2023-01-09 22:22:26 -0300

    integracao com o front

--- app.js
+++ app.js
@@ -5,10 +5,31 @@ const cors = require('cors')
 app.use(express.json())
 app.use(cors())
 
-
 require('dotenv/config')
 
+const produto =[ 
+    {
+        id: 1,
+        nome: 'celular',
+        preco: 140,
+    }
+]
+
 const api = process.env.API_URL
+
+app.get(`${api}/produtos`, (req, res)=>{
+    res.send(produto)
+})
+
+app.post(`${api}/produtos`, (req, res)=>{
+    console.log(req.body)
+
+    const novoProduto = req.body
+
+    produto.push(novoProduto)
+
+    res.status(201).json(novoProduto)
+})
 
 app.listen(8080, () =>{
     console.log("Servidor rodando na porta 8080")
